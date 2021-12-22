53fc6d34a8599ccf528daa227df585bc825f0ea7
Jorge Ortiz <ortiz.mata.jorge@gmail.com>
2021-12-22 13:19:52 -0600

    We added information of the project. You can check it out in the javascript file.

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 
+// ----------------------------------------------------------- PROJECT 3. BACKGROUND COLOR PROJECT. -------------------------------------------------------------
+
+
+// ING. Jorge Ortiz Mata.
+// ortiz.mata.jorge@gmail.com
+// San Luis Potosí S.L.P. México.
+// (444) 576 3034.
+
+// Hello! I'm Jorge. Today, I'm gonna show you this project that I built.
+// Once you picked the button below of this section, the background color will change. 
+// It'll give you a random color and you can have it either on hexadecimal or in simple form.
+// Come on, and have fun with this project.
+
+// ---------------  CREATE VARIABLES ------------------
+
 const colors = [
     'orange',
     'blue',
@@ -12,20 +27,32 @@ const colors = [
     'brown'
 ];
 
+// ---------------  SELECT OBJETCS ------------------
+
 const backgroundObjects = document.querySelector ('.backgroundObjects');
 const backgroundProject = document.querySelector ('.backgroundProject');
+
+// ---------------  CREATE OBJETCS ------------------
 
 const buttonBackgroundChange = document.createElement ('button');
 const colorBackgroundTitle = document.createElement ('colorBackgroundTitle');
 
+// ---------------  ADDING CLASES ------------------
+
 buttonBackgroundChange.classList.add ('buttonBackgroundChange');
 colorBackgroundTitle.classList.add ('colorBackgroundTitle');
+
+// ---------------  APPEND OBJETCS ------------------
 
 backgroundObjects.appendChild (colorBackgroundTitle);
 backgroundObjects.appendChild (buttonBackgroundChange);
 
+// ---------------  ADDING TEXTS ------------------
+
 colorBackgroundTitle.textContent = `default`;
 buttonBackgroundChange.innerHTML = `Click me to change.`;
+
+// ---------------  CREATING FUNCTIONS ------------------
 
 changeColor = () => {
 
@@ -35,5 +62,7 @@ changeColor = () => {
     console.log (x);
 
 }
+
+// ---------------  ADDING EVENTS LISTENERS ------------------
 
 buttonBackgroundChange.addEventListener ('click', changeColor);
