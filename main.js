817b3805cd562fd10c9138455bc925d7b06b72d3
Jorge Ortiz <ortiz.mata.jorge@gmail.com>
2021-12-22 13:14:04 -0600

    Javascript code was developed. It begins with only normal colors.

--- main.js
+++ main.js
@@ -6,9 +6,14 @@ const colors = [
     'red',
     'pink',
     'yellow',
+    'antiquewhite',
+    'aquamarine',
+    'aqua',
+    'brown'
 ];
 
 const backgroundObjects = document.querySelector ('.backgroundObjects');
+const backgroundProject = document.querySelector ('.backgroundProject');
 
 const buttonBackgroundChange = document.createElement ('button');
 const colorBackgroundTitle = document.createElement ('colorBackgroundTitle');
@@ -19,5 +24,16 @@ colorBackgroundTitle.classList.add ('colorBackgroundTitle');
 backgroundObjects.appendChild (colorBackgroundTitle);
 backgroundObjects.appendChild (buttonBackgroundChange);
 
-colorBackgroundTitle.innerHTML = `default`;
+colorBackgroundTitle.textContent = `default`;
 buttonBackgroundChange.innerHTML = `Click me to change.`;
+
+changeColor = () => {
+
+    let x = Math.floor(Math.random() * 10);
+    backgroundProject.style.backgroundColor = `${colors[x]}`;
+    colorBackgroundTitle.innerHTML = `${colors[x]}`;
+    console.log (x);
+
+}
+
+buttonBackgroundChange.addEventListener ('click', changeColor);
